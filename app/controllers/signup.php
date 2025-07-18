63b147d0ab8a42cdc0a5881207ac6a29a4de8813
Matt <mdelfierro@algomau.ca>
2025-07-18 23:32:42 +0000

    Beginning the reports

--- app/controllers/signup.php
+++ app/controllers/signup.php
@@ -1,8 +1,15 @@
 <?php
 
 class signup extends Controller {
+
+  
   public function index(){
     $this->view('signup/index');
+  }
+
+  public function create(){
+    $new_user = $this->model('User');
+    
   }
 }
 
---
+++ app/views/reports/chart.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+<title>Reminder Chart/Table</title>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+  <div class="container">
+    <h1 class="text-center">Reminders By Users</h1>
+    <canvas id="allReminders" width="75" height="75"></canvas>  
+  </div>
