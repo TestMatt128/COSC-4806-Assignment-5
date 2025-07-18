31afd85e9a5f9743448ba2f2ff45f98c2dd2d4a7
Matt <mdelfierro@algomau.ca>
2025-07-18 23:36:23 +0000

    Imports of bootstrap

--- app/views/signup/index.php
+++ app/views/signup/index.php
@@ -1,3 +1,9 @@
 <?php require_once 'app/views/templates/header.php' ?>
 
 <?php require_once 'app/views/templates/footer.php' ?>
+
+<!--Begin the bootstrap imports -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Chart.js -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
