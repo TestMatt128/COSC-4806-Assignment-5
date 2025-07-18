424ec47bceb73a6eec13fe44cb921648d2bc3b28
Matt <mdelfierro@algomau.ca>
2025-07-18 22:29:50 +0000

    Readjustments

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -8,6 +8,9 @@ class Controller {
     }
     
     public function view ($view, $data = []) {
+        foreach ($data as $key => $value){
+            $$key = $value; //$$key is a variable, ['reminders' => $list_reminders] into $reminders
+        }
         require_once 'app/views/' . $view .'.php';
     }
 
