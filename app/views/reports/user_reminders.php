6afb238c5a25dd45b40e873af348f9a33fc59966
Matt <mdelfierro@algomau.ca>
2025-07-19 03:55:06 +0000

    Final commit - Started from scratch from Assignment 3
    For context, I rebuilt everything from the ground-up and linked parts of A4 to here.

--- app/views/reports/user_reminders.php
+++ app/views/reports/user_reminders.php
@@ -1,5 +1,12 @@
 <?php require_once 'app/views/templates/header.php' ?>
 
-
+<div class="container mt-5">
+<nav aria-label="breadcrumb" class="mb-3">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="/">Home</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Reports</li>
+  </ol>  
+</nav>
+<h2>Reminders by All Users</h2>
 
 <?php require_once 'app/views/templates/footer.php' ?>
