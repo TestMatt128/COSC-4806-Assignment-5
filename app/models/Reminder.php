f1116bf1f3c2968b921e33fd49b6da6ff0b97849
Matt <mdelfierro@algomau.ca>
2025-07-19 03:42:01 +0000

    More functions

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -7,10 +7,14 @@ class Reminder {
   }
   public function getReminders(){
     $db = db_connect();
-    $statement = $db->prepare("SELECT reminders.*, users.username FROM reminders JOIN users ON reminders.user_id = users.id ORDER BY reminders.time_created ASC;");
+    $statement = $db->prepare("SELECT reminders.*, users.username FROM reminders JOIN users ON reminders.user_id = users.id ORDER BY reminders.created_at DESC");
     $statement->execute();
     $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $rows;
+  }
+  public function createReminder($user_id, $subject){
+    $db = db_connect();
+    $statement = $db->prepare("INSERT INTO reminders (user_id, subject, created_at) VALUES(:user_id, :subject, NOW(), 0)");
   }
   
 }
