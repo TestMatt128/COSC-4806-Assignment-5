5beeab620dc9920fd6ee533016ebc9f4bffb1eb6
Matt <mdelfierro@algomau.ca>
2025-07-19 03:44:51 +0000

    More touches

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -15,6 +15,24 @@ class Reminder {
   public function createReminder($user_id, $subject){
     $db = db_connect();
     $statement = $db->prepare("INSERT INTO reminders (user_id, subject, created_at) VALUES(:user_id, :subject, NOW(), 0)");
+    $statement->bindValue(':user_id', $user_id);
+    $statement->bindValue(':subject', $subject);
+    return $statement->execute()
+    
+  }
+  public function getReminderByid($id){
+    $db = db_connect();
+    $statement = $db->prepare("SELECT * FROM reminders WHERE id = :id");
+    $statement->bindValue(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
+    return $statement->fetch(PDO::FETCH_ASSOC)
+  }
+  public function updateReminder($id, $subject){
+    $db = db_connect();
+    $statement = $db->prepare("UPDATE reminders SET subject = :subject WHERE id = :id");
+    $statement->bindValue(':id', $id, PDO::PARAM_INT);
+    $statement->bindValue(':subject', $subject, PDO::PARAM_STR);
+    $statement->execute();
   }
   
 }
