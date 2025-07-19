387eecbd2e6c04e294f8b6cf3ea554206a5375b0
Matt <mdelfierro@algomau.ca>
2025-07-19 03:03:46 +0000

    sign-in

--- app/controllers/signup.php
+++ app/controllers/signup.php
@@ -1,15 +1,35 @@
 <?php
 
-class signup extends Controller {
-
+class Signup extends Controller {
   
   public function index(){
-    $this->view('signup/index');
+    $message = $_SESSION['signup_message'] ?? '';
+    unset($_SESSION['signup_message']);
+    $this->view('Signup/index',['message' => $message]);
   }
 
   public function create(){
     $user = $this->model('User');
+
+    // Password needs to be at least 7 characters long for it to be strong.
+    function strong_password($password){
+      return length($password) >= 7;
+    }
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
+
+    if(!empty($username) && !empty($password)){
+       if(!strong_password($password)) {
+          $_SESSION['signup_message'] = 'Password must be at least 7 characters long.';
+         header('Location: /signup');
+         exit;
+       }
+       elseif($user->$existing_username($username)){
+         
+       }
+    }
     
+    exit;
   }
 }
 
